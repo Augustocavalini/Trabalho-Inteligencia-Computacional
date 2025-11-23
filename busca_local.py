70443b4cec40ba92b4a08c80dce79a5e2ab10d9d
Augustocavalini <augustocavalini.filippo@estudante.ufjf.br>
2025-11-23 20:18:51 -0300

    Add new local search heuristics for job scheduling
    Introduced three new functions: deterministic_job_selection, double_job_exchange, and double_job_insert, to enhance local search strategies for job scheduling. Also updated insert_block_best to dynamically set max_block_size based on sequence length. These changes aim to improve solution quality by providing additional perturbation and neighborhood exploration methods.

--- busca_local.py
+++ busca_local.py
@@ -249,7 +249,7 @@ def insert_block_random(inst: Instance, res: List[int]) -> Tuple[List[int], Dict
     return best_sol, best_solution, improved
 
 
-def insert_block_best(inst: Instance, res: List[int], min_block_size: int = 2, max_block_size: int = 3) -> Tuple[List[int], Dict]:
+def insert_block_best(inst: Instance, res: List[int], min_block_size: int = 2) -> Tuple[List[int], Dict]:
     
     print("\nIniciando Perturbação Insert Block Best...")
     
@@ -269,7 +269,7 @@ def insert_block_best(inst: Instance, res: List[int], min_block_size: int = 2, m
         interation += 1
         
         # Seleciona o tamanho do bloco
-        block_size = random.randint(min_block_size, max_block_size)
+        block_size = random.randint(min_block_size, int(0.4 * len(best_sol)))
         
         if len(best_sol) < block_size:
             continue
@@ -304,3 +304,152 @@ def insert_block_best(inst: Instance, res: List[int], min_block_size: int = 2, m
     print("Número de tentativas:", interation)
     
     return best_sol, best_solution, improved
+
+def deterministic_job_selection(inst: Instance, res: List[int]) -> List[int]:
+   
+    print("\nIniciando Seleção Determinística de Tarefas...")
+    best_solution = res
+    best_sol = res["sequence_normalized"]
+    best_sol_makespan = best_solution["C_max"]
+    
+    improved = False
+    
+    best_insertion_sol = None
+    best_insertion_makespan = float('inf')
+
+    interation = 0
+
+    while best_sol_makespan < best_insertion_makespan:
+        interation += 1
+        
+        for job in best_sol:
+            # Remove a tarefa da sequência
+            new_sol = [j for j in best_sol if j != job]
+            
+            # Tenta inserir a tarefa em todas as posições possíveis
+            for insert_index in range(len(new_sol) + 1):
+                trial_sol = new_sol[:insert_index] + [job] + new_sol[insert_index:]
+                trial_solution = verify_solution(inst, trial_sol, verbose=False)
+                
+                if trial_solution["feasible"] and trial_solution["C_max"] < best_insertion_makespan:
+                    best_insertion_makespan = trial_solution["C_max"]
+                    best_insertion_sol = trial_sol
+                    best_insertion_solution = trial_solution            
+            
+        if best_insertion_sol is not None and best_insertion_makespan < best_sol_makespan:
+            best_sol = best_insertion_sol
+            best_solution = best_insertion_solution
+            best_sol_makespan = best_insertion_makespan
+            print(f"Perturbação realizada: job movido para melhor posição, tentativa {interation}")
+            improved = True
+            
+    print("Perturbação Insert Block Best finalizada.")
+    print("Número de tentativas:", interation)
+    
+    return best_sol, best_solution, improved   
+   
+    
+def double_job_exchange(inst: Instance, res: List[int]) -> Tuple[List[int], Dict]:
+    print("\nIniciando Job Exchange...")
+    
+    best_solution = res
+    best_sol = res["sequence_normalized"]    
+    
+    best_makespan = best_solution["C_max"]
+    improved = False
+
+    max_stagnation = 1000  # Define um número máximo de iterações para evitar loops infinitos
+    stagnation_counter = 0
+    iterations = 0
+
+    while iterations < max_stagnation:
+
+        i_1 = random.randint(0, len(best_sol) - 1)
+        i_2 = random.randint(0, len(best_sol) - 1)
+        j_1 = random.randint(0, len(best_sol) - 1)
+        j_2 = random.randint(0, len(best_sol) - 1)
+        
+        if i_1 == j_1 or i_1 == j_2 or i_2 == j_1 or i_2 == j_2:
+            continue
+        
+        new_sol = best_sol[:]
+        new_sol[i_1], new_sol[j_1] = new_sol[j_1], new_sol[i_1]
+        new_sol[i_2], new_sol[j_2] = new_sol[j_2], new_sol[i_2]
+        
+        new_solution = verify_solution(inst, new_sol, verbose=False)
+        if new_solution["feasible"]:
+            new_makespan = new_solution["C_max"]
+            
+            if new_makespan < best_makespan:
+                best_sol = new_sol
+                best_solution = new_solution
+                best_makespan = new_makespan
+                improved = True
+                print(f"Melhoria encontrada: novo makespan {new_makespan} trocando posições {i_1} e {j_1}, {i_2} e {j_2}, tentativa {stagnation_counter}")
+                stagnation_counter = 0
+
+            
+            else:
+                stagnation_counter += 1
+        iterations += 1
+                
+    
+    print("Busca Local finalizada. Achou um ótimo local.")
+    print("Número de iterações:", iterations)
+    
+    return  best_sol, best_solution, improved
+
+def double_job_insert(inst: Instance, res: List[int]) -> Tuple[List[int], Dict]:
+    print("\nIniciando Double Job Insert...")
+    
+    best_solution = res
+    best_sol = res["sequence_normalized"]    
+    
+    best_makespan = best_solution["C_max"]
+    improved = False
+
+    max_stagnation = 1000  # Define um número máximo de iterações para evitar loops infinitos
+    stagnation_counter = 0
+    iterations = 0
+
+    while iterations < max_stagnation:
+
+        remove_index_1 = random.randint(0, len(best_sol) - 1)
+        remove_index_2 = random.randint(0, len(best_sol) - 1)
+        
+        if remove_index_1 == remove_index_2:
+            continue
+        
+        job_1 = best_sol[remove_index_1]
+        job_2 = best_sol[remove_index_2]
+        
+        new_sol = [j for idx, j in enumerate(best_sol) if idx not in (remove_index_1, remove_index_2)]
+        
+        insert_index_1 = random.randint(0, len(new_sol))
+        new_sol = new_sol[:insert_index_1] + [job_1] + new_sol[insert_index_1:]
+        
+        insert_index_2 = random.randint(0, len(new_sol))
+        new_sol = new_sol[:insert_index_2] + [job_2] + new_sol[insert_index_2:]
+        
+        new_solution = verify_solution(inst, new_sol, verbose=False)
+        if new_solution["feasible"]:
+            new_makespan = new_solution["C_max"]
+            
+            if new_makespan < best_makespan:
+                best_sol = new_sol
+                best_solution = new_solution
+                best_makespan = new_makespan
+                improved = True
+                print(f"Melhoria encontrada: novo makespan {new_makespan} movendo jobs para posições {insert_index_1} e {insert_index_2}, tentativa {stagnation_counter}")
+                stagnation_counter = 0
+
+            
+            else:
+                stagnation_counter += 1
+        iterations += 1
+                
+    
+    print("Busca Local finalizada. Achou um ótimo local.")
+    print("Número de iterações:", iterations)
+    
+    return  best_sol, best_solution, improved
